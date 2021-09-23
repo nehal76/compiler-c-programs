28627c07b197bff59abf0a12ad9ce2a3f300951a
nehal76 <91207110+nehal76@users.noreply.github.com>
2021-09-23 11:18:20 +0600

    Create wether chechk string is constant or not@nehal.c

---
+++ wether chechk string is constant or not@nehal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int i, len, n = 0;
+char str[10];
+clrscr();
+printf("Nehal Ahmed-2001330109015\n\nEnter a string:");
+gets(str);
+   len = strlen(str);
+   while(n<len)
+   {
+   if(isdigit(str[i]))
+   {
+   }
+   else
+   {
+   }
+   n++;
+   printf("it is not a constant");
+   if(n==len)
+   printf("\nit is not a constant:");
+   getch();
+   }
