99b4adb104a48f235a376cde36581e28a51b6d7e
nehal76 <91207110+nehal76@users.noreply.github.com>
2021-09-23 11:20:32 +0600

    Create whether string is keyword or not@nehal.c

---
+++ whether string is keyword or not@nehal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+  int i,n, flag = 0;
+  char str[10];
+  char key[7][10] = {"goto", "exit", "break", "if", "else", "contine", "return",};
+  clrscr();
+  printf("Nehal ahmed-2001330109015\n\nEnter a string ");
+  gets(str);
+  for(i=0; i<7; i++)
+  {
+  n = strcmp(str, key[i]);
+  if(n==0)
+  flag = 1;
+  }
+  if(flag == 0)
+  printf("\nIt is not a keyword");
+  else
+  printf("\n It is a keyword");
+  getch();
+  return;
+  }
