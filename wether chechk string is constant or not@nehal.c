902fb93560ab207f405058849121b65a722ffafb
nehal76 <91207110+nehal76@users.noreply.github.com>
2021-09-30 19:42:19 +0530

    Update wether chechk string is constant or not@nehal.c

--- wether chechk string is constant or not@nehal.c
+++ wether chechk string is constant or not@nehal.c
@@ -1,24 +1,20 @@
-#include <stdio.h>
-#include <conio.h>
-void main()
-{
-int i, len, n = 0;
-char str[10];
-clrscr();
-printf("Nehal Ahmed-2001330109015\n\nEnter a string:");
-gets(str);
-   len = strlen(str);
-   while(n<len)
-   {
-   if(isdigit(str[i]))
-   {
-   }
-   else
-   {
-   }
-   n++;
-   printf("it is not a constant");
-   if(n==len)
-   printf("\nit is not a constant:");
-   getch();
-   }
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<ctype.h>
+void main() {
+ char s[100];
+ int i, flag=0, n=0;
+
+printf("Nehal Ahmed-2001330109015\nEnter the string:\n");
+scanf("%s", &s);
+ for(i=0;s[i]!='\0';i++){
+if(isdigit(s[i])==0)
+ flag++;
+ }
+ if(flag==0)
+ printf("Given String is constant, contains only numbers!");
+ else
+ printf("Given string isn't constant");
+ getch();
+}
